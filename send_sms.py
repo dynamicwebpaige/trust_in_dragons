9904aaa63d4c650561d98305e9014eb7925b64f4
Paige Bailey <profoundlypaige@gmail.com>
2017-06-07 14:36:13 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -3,11 +3,11 @@ from twilio.rest import TwilioRestClient
  
 # Your Account Sid and Auth Token from twilio.com/user/account
 def send_one_sms(phoneNum):
-    account_sid = "ACbed2373fd3953a3779dc6de74d1300df"
-    auth_token  = "1410cc3ba58b8e555c94e91409a3e351"
+    account_sid = ""
+    auth_token  = ""
     client = TwilioRestClient(account_sid, auth_token)
      
     message = client.sms.messages.create(body="Your donation to XXXX was succesfully used to purchase XXX",
-        to=phoneNum, #"+15126059304",    # Replace with your phone number
-        from_="+15122706308") # Replace with your Twilio number
+        to=phoneNum, #"",    # Replace with your phone number
+        from_="") # Replace with your Twilio number
     print message.sid
